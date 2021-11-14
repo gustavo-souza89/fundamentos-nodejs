5ede5fafca3b371cb8f4428f851ab6f13c6064e6
Gustavo Souza <gustavo.souzagu@gmail.com>
2021-11-14 08:30:09 -0300

    Buscando extrato bancario

--- src/index.js
+++ src/index.js
@@ -13,22 +13,39 @@ const customers = [];
  * id - uuid
  * statement []
  */
+//Criação da Conta - Validando CPF
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
-
-const customersAlreadyExists = customers.some((customers) => customers.cpf === cpf);
-
-if (customersAlreadyExists) {
-    return response.status(400).json({error: "Customer already exists!"});
-}    
-    customers.push({
-        cpf,
-        name,
-        id: uuidv4(),
-        statement: [],
+    
+    //validação CPF
+    const customersAlreadyExists = customers.some((customers) => customers.cpf === cpf);
+    
+    //RetornoERROR - CPF já Cadastrado
+    if (customersAlreadyExists) {
+        return response.status(400).json({error: "Customer already exists!"});
+    }    
+        customers.push({
+            cpf,
+            name,
+            id: uuidv4(),
+            statement: [],
+        });
+        //Retorno do Cadastro
+        return response.status(201).send();
     });
 
-    return response.status(201).send();
+
+//Busca do extrato bancário
+app.get("/statement", (request, response) => {
+    //Obs: headers utilizado no lugar de params pq futuramente usará tokens no headers
+    const{cpf} = request.headers;
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer) {
+        return response.status(400).json({error: "Customer not found"})
+    }
+
+    return response.json(customer.statement);
 });
 
 app.listen(3333);
